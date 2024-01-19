742ab8a49bed8e9ed98d52b0e628a0a69f09f2cf
Islam Hamdaoui <91889739+islamhamdaoui@users.noreply.github.com>
2024-01-19 13:35:03 +0100

    Update App.js and added username and date

--- App.js
+++ App.js
@@ -5,56 +5,49 @@ const appSettings = {
   databaseURL: "https://notes-50046-default-rtdb.europe-west1.firebasedatabase.app/"
 }
 
-let app = initializeApp(appSettings)
-let database = getDatabase(app)
-let noteInsert=  ref(database, "notes")
+let app = initializeApp(appSettings);
+let database = getDatabase(app);
+let noteInsert=  ref(database, "notes");
 
 let input =document.getElementById("input")
 let button = document.getElementById("btn")
 let list = document.getElementById("list")
 let error = document.getElementById("error")
 
+let usernameInput = document.getElementById("username");
 
 
 
 
 
-button.addEventListener("click" , function(){
-  let inputValue = input.value
-  input.value= ""
-  if (inputValue !== "") {
-     push(noteInsert,inputValue)
+button.addEventListener("click", function() {
+  let inputValue = input.value.trim();
+  let usernameValue = usernameInput.value.trim();
+  if (inputValue !== "" && usernameValue !== "") {
+    const currentDate = new Date().toLocaleString(undefined, {
+      year: "numeric",
+      month: "numeric",
+      day: "numeric",
+      hour: "numeric",
+      minute: "numeric"
+    });
+    push(noteInsert, { note: inputValue, date: currentDate, username: usernameValue });
+    input.value = "";
+    usernameInput.value = "";
   }
-
-})
-
-
+});
 onValue(noteInsert, function(snapshot) {
   if (snapshot.exists()) {
-  
-    let things = Object.values(snapshot.val())
-  
-    list.innerHTML =""
-  
-      for (let i = 0; i < things.length; i++) {
-  
-        list.innerHTML += 
-        `<li>${things[i]}</li>`
-  
-      }
-  
+    let notes = Object.values(snapshot.val());
+    list.innerHTML = "";
+
+    for (let i = 0; i < notes.length; i++) {
+      const { note, date, username } = notes[i];
+        list.innerHTML += `<div class="note"><b>${username}</b><li>${note}</li><p>${date}</p></div>`;
+    } 
+
   } else {
-  
-    list.innerHTML = "No things yet"
-  
+    list.innerHTML = "No notes yet";
   }
-})
-
-
-input.addEventListener("input",function(){
-  error.style.display = "none"
-})
-
-
-
+});
 
